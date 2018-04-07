4244bfff940bd71671d13964cdd2fc1bab2cbba5
samvicky <billavignesh26@gmail.com>
2018-04-08 00:33:57 +0530

    Update slow-hash.c

--- src/crypto/slow-hash.c
+++ src/crypto/slow-hash.c
@@ -53,8 +53,8 @@ void cn_slow_hash_f(void * a, const void * b, size_t c, void * d){
 #define restrict
 #endif
 
-#define MEMORY         (1 << 21) /* 2 MiB */
-#define ITER           (1 << 20)
+#define MEMORY         (1 << 22) /* 4 MiB */
+#define ITER           (1 << 21)
 #define AES_BLOCK_SIZE  16
 #define AES_KEY_SIZE    32 /*16*/
 #define INIT_SIZE_BLK   8
